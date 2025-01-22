1870fc08638f07fe883ef6577341320f38f9f432
Ptakk1 <lubiepalski@gmail.com>
2025-01-22 18:36:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function hideElements() {
+    document.querySelector('.kop1').style.display = 'none';
+    document.querySelector('.kop2').style.display = 'none';
+    document.querySelector('.koperta').style.display = 'none';
+    document.querySelector('.open').style.display = 'none';
+    document.querySelector('.zaslona').style.display = 'none';
+    document.querySelector('.content').style.display = 'none';
+    document.querySelector('.kartka').style.display = 'flex';
+    document.querySelector('.ansewr').style.display = 'block';
+}
+
+
+function part1() {
+    document.querySelector('.kartka').style.display = 'none';
+    document.querySelector('.part1').style.display = 'none';
+    document.querySelector('.open2').style.display = 'none';
+    document.querySelector('.submit').style.display = 'block';
+    document.querySelector('.part2').style.display = 'block';
+    
+}
